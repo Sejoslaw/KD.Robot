90f3c29d1f1289bdb64a28a559148792d19bf033
Krzysztof Dobrzynski <k.dobrzynski94@gmail.com>
2017-07-31 20:08:16 +0200

    Initial Commit

--- KD.Robot/Properties/AssemblyInfo.cs
+++ KD.Robot/Properties/AssemblyInfo.cs
@@ -1,23 +1,22 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("KD.Robot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Robot which will do some work for You in background.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KD.Robot")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Krzysztof Dobrzynski 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f7506f8c-c618-4cb8-a748-8d359b10774d")]
